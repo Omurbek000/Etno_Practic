a3c18389bc2b900cbdfd371aea68c8110392a391
Omurbek <dseba436@gmail.com>
2026-05-30 01:23:19 +0700

    add model
    I added models, filled the database, and made migrations

--- movie/etno/admin.py
+++ movie/etno/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
-
+from .models import *
 # Register your models here.
+
+admin.site.register(UserProfile)
+admin.site.register(Genre)
+admin.site.register(Person)
+admin.site.register(Country)
+admin.site.register(Film)
+admin.site.register(Series)
+admin.site.register(Season)
+admin.site.register(Cartoon)
+admin.site.register(Subscription)
+admin.site.register(Favorite)
+admin.site.register(FavoriteItem)
+admin.site.register(Review)
--- movie/etno/models.py
+++ movie/etno/models.py
@@ -4,6 +4,22 @@ from phonenumber_field.modelfields import PhoneNumberField
 from django.core.validators import MinValueValidator, MaxValueValidator
 
 
+LANGUAGE_CHOICES = (
+        (" Kyrgyz", "Kyrgyz"),
+        ("Russian", "Russian"),
+        ("Other", "Other"),
+    )
+
+ACCESS_TYPE = (("Free", "Free"), ("Subscription", "Subscription"), ("Rent", "Rent"))
+
+AGE_CHOICES = (
+    ('0+', '0+'),
+    ('6+', '6+'),
+    ('12+', '12+'),
+    ('16+', '16+'),
+    ('18+', '18+')
+)
+
 class UserProfile(AbstractUser):
     avatar = models.ImageField(upload_to="profile_image", null=True, blank=True)
     phone_number = PhoneNumberField(
@@ -17,6 +33,9 @@ class UserProfile(AbstractUser):
         max_length=5, choices=STATUS_CHOICES, default="Free"
     )
     subscription_end = models.DateTimeField(null=True, blank=True)
+    
+    def __str__(self) -> str:
+        return self.username
 
 
 class Genre(models.Model):
@@ -27,6 +46,10 @@ class Genre(models.Model):
         null=True,
         blank=True,
     )
+    
+    def __str__(self):
+        return self.name
+    
 
 
 class Person(models.Model):
@@ -40,11 +63,17 @@ class Person(models.Model):
         ("both", "both"),
     )
     role = models.CharField(max_length=10, choices=ROLE_CHOICES)
+    
+    def __str__(self):
+        return f'{self.first_name} - {self.last_name}, {self.role}'
+    
 
 
 class Country(models.Model):
     country = models.CharField(max_length=100, unique=True)
 
+    def __str__(self):
+        return self.country
 
 class Film(models.Model):
     title = models.CharField(max_length=100)
@@ -53,20 +82,111 @@ class Film(models.Model):
     year = models.PositiveIntegerField()
     country = models.ForeignKey(Country, on_delete=models.CASCADE)
     duration = models.PositiveIntegerField()
-    LANGUAGE_CHOICES = (
-        (" Kyrgyz", "Kyrgyz"),
-        ("Russian", "Russian"),
-        ("Other", "Other"),
-    )
     language = models.CharField(max_length=10, choices=LANGUAGE_CHOICES)
     video = models.FileField(upload_to="film_video")
     trailer = models.URLField()
     genres = models.ManyToManyField(Genre)
     persons = models.ManyToManyField(Person)
-    ACCESS_TYPE = (("Free", "Free"), ("Subscription", "Subscription"), ("Rent", "Rent"))
     access_type = models.CharField(max_length=15, choices=ACCESS_TYPE)
     rent_price = models.DecimalField(max_digits=10, decimal_places=2)
     is_published = models.BooleanField(default=False)
     views_count = models.PositiveBigIntegerField()
     created_date = models.DateField(auto_now_add=True)
+    
+    def __str__(self):
+        return self.title
+    
+
+    
+class Series(models.Model):
+    title = models.CharField(max_length=30)
+    description = models.TextField(null=True, blank=True)
+    series_image = models.ImageField(upload_to="series_image")
+    year = models.PositiveIntegerField()
+    country = models.ForeignKey(Country, on_delete=models.CASCADE)
+    language = models.CharField(max_length=15, choices=LANGUAGE_CHOICES)
+    trailer_url = models.URLField()
+    genres = models.ManyToManyField(Genre)
+    persons = models.ManyToManyField(Person)
+    access_type = models.CharField(max_length=15, choices=ACCESS_TYPE)
+    is_published = models.BooleanField(default=False)
+    views_count = models.PositiveBigIntegerField()
+    created_date = models.DateField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
+    
+
+
+class Season(models.Model):
+    serise = models.ForeignKey(Series, on_delete=models.CASCADE)
+    season_number = models.PositiveIntegerField()    
+    title = models.CharField(max_length=100)
+    year = models.PositiveIntegerField()
+ 
+ 
+    def __str__(self):
+        return self.serise
+    
+
+
+class Cartoon(models.Model):
+    title = models.CharField(max_length=30)
+    description = models.TextField(null=True, blank=True)
+    cartoon_image = models.ImageField(upload_to='cartoon_image')
+    year = models.PositiveIntegerField()
+    country = models.ForeignKey(Country, on_delete=models.CASCADE)
+    language = models.CharField(max_length=15, choices=LANGUAGE_CHOICES)
+    duration = models.PositiveIntegerField()
+    video = models.FileField(upload_to="film_video")
+    trailer_url = models.URLField()
+    age_rating = models.CharField(max_length=10, choices=AGE_CHOICES)
+    genres = models.ManyToManyField(Genre)
+    access_type = models.CharField(max_length=15, choices=ACCESS_TYPE)
+    is_published = models.BooleanField(default=False)
+    views_count = models.PositiveBigIntegerField()
+    created_date = models.DateField(auto_now_add=True)
+    
+    def __str__(self):
+        return self.title
+    
+
+
+class Subscription(models.Model):
+    user = models.ForeignKey(UserProfile, on_delete=models.CASCADE)
+    PLAN_CHOICES = (
+        ('daily', 'daily'),
+        ('monthly', 'monthly'),
+        ('year', 'year')
+    )
+    plan = models.CharField(max_length=15, choices=PLAN_CHOICES)
+    start_date = models.DateTimeField(auto_now_add=True)
+    end_date = models.DateTimeField()
+    is_activ = models.BooleanField(default=False)
+    price = models.PositiveIntegerField()
+ 
+    
+class Favorite(models.Model):
+    user = models.OneToOneField(UserProfile, on_delete=models.CASCADE)
+
+
+class FavoriteItem(models.Model):
+    watchlist = models.ForeignKey(Favorite, on_delete=models.CASCADE)
+    film = models.ForeignKey(Film, on_delete=models.CASCADE, null=True, blank=True)
+    series = models.ForeignKey(Series, on_delete=models.CASCADE, null=True, blank=True)
+    cartoon = models.ForeignKey(Cartoon, on_delete=models.CASCADE, null=True, blank=True)
+    added_date = models.DateTimeField(auto_now_add=True)
+    
+
+
+class Review(models.Model):
+    user = models.ForeignKey(UserProfile, on_delete=models.CASCADE)
+    film = models.ForeignKey(Film, on_delete=models.CASCADE, null=True, blank=True)
+    series = models.ForeignKey(Series, on_delete=models.CASCADE, null=True, blank=True)
+    cartoon = models.ForeignKey(Cartoon, on_delete=models.CASCADE, null=True, blank=True)
+    stars = models.PositiveSmallIntegerField(choices=[(i, str(i)) for i in range(1, 11)],null=True,blank=True,)
+    text = models.TextField(null=True, blank=True)
+    parent = models.ForeignKey('self', on_delete=models.CASCADE, null=True, blank=True)
+    created_date = models.DateTimeField(auto_now_add=True)
+    
     
