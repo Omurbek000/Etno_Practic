2c63c49619f71dabbd19c807b5681ed4acac4f2f
Omurbek <dseba436@gmail.com>
2026-06-27 23:55:38 +0700

    deleted front

--- movie/etno/models.py
+++ movie/etno/models.py
@@ -235,8 +235,3 @@ class Review(models.Model):
         ordering = ["-created_date"]
 
 
-class Testke(models.Model):
-    name = models.TextField()
-    age = models.IntegerField()
-    first_name = models.TextField()
-    created_at = models.DateField(auto_created=True)
